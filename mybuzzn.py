de0ac7011c936567925d2ab7698bf3f0b4b9f65f
Judith Greif <obermaedchen@gmail.com>
2019-10-11 12:41:20 +0200

    Make example minimal

--- mybuzzn.py
+++ mybuzzn.py
@@ -1,13 +1,11 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO, Namespace, emit
-
-async_mode = None
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 
 # Enable encryption
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
-socketio = SocketIO(app, async_mode=async_mode)
+socketio = SocketIO(app)
 
 @app.route('/')
 def index(): 
@@ -18,11 +16,11 @@ def on_my_event(message):
     socketio.emit('my response', {'data': message['data']})
 
 @socketio.on('connect', namespace='/mybuzzn')
-def test_connect():
+def connect():
     emit('my response', {'data': 'Connected'})
 
 @socketio.on('disconnect', namespace='/mybuzzn')
-def test_disconnect():
+def disconnect():
     print('Client disconnected')
 
 if __name__ == '__main__': 
