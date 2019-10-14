12276daa0b19ed9ae5c7b2f5e88ab0134195d63b
Judith Greif <obermaedchen@gmail.com>
2019-10-14 12:41:48 +0200

    Create random server sent data, cleanup code

--- mybuzzn.py
+++ mybuzzn.py
@@ -1,10 +1,10 @@
+import json
+import pytz
+from numpy import random
+from datetime import datetime
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 from threading import Lock
-from flask import Flask, render_template, session, request, \
-    copy_current_request_context
-from flask_socketio import SocketIO, emit, join_room, leave_room, \
-    close_room, rooms, disconnect
 
 # Set this variable to "threading", "eventlet" or "gevent" to test the
 # different async modes, or leave it set to None for the application to choose
@@ -18,90 +18,52 @@ thread = None
 thread_lock = Lock()
 
 
+def generate_live_data():
+    date = (datetime.utcnow().replace(tzinfo=pytz.utc)).strftime("%Y-%M-%d %H:%M:%S%z")
+    userConsumption = random.randint(0, 4000)
+    groupConsumption = random.randint(0, 50000)
+    selfSufficiency = random.randint(0, 101)
+    user_icon = "testuser"
+    user_name = "Harry Potter"
+    user_level = random.randint(0, 100)
+    group_icon = "testgroup"
+    group_name = "Hogwarts"
+    group_level = random.randint(0, 100)
+    data = {
+            "date": date,
+            "userConsumption": userConsumption,
+            "groupConsumption": groupConsumption,
+            "selfSufficiency": selfSufficiency,
+            "consumersUser": {
+                              "icon": user_icon, 
+                              "name": user_name, 
+                              "level": user_level
+                             },
+            "consumersGroup": {
+                               "icon": group_icon, 
+                               "name": group_name, 
+                               "level": group_level
+                              }
+           }
+    return json.dumps(data)
+
+
 def background_thread():
-    """Example of how to send server generated events to clients."""
+    """Example of how to send server generated events to clients.
+    Emit live data every 10 seconds."""
     count = 0
     while True:
         socketio.sleep(10)
         count += 1
+        live_data = generate_live_data()
         socketio.emit('my_response',
-                      {'data': 'Server generated event', 'count': count},
+                      {'data': live_data, 'count': count},
                       namespace='/test')
 
 
 @app.route('/')
 def index():
     return render_template('index.html', async_mode=socketio.async_mode)
-
-
-@socketio.on('my_event', namespace='/test')
-def test_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']})
-
-
-@socketio.on('my_broadcast_event', namespace='/test')
-def test_broadcast_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']},
-         broadcast=True)
-
-
-@socketio.on('join', namespace='/test')
-def join(message):
-    join_room(message['room'])
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': 'In rooms: ' + ', '.join(rooms()),
-          'count': session['receive_count']})
-
-
-@socketio.on('leave', namespace='/test')
-def leave(message):
-    leave_room(message['room'])
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': 'In rooms: ' + ', '.join(rooms()),
-          'count': session['receive_count']})
-
-
-@socketio.on('close_room', namespace='/test')
-def close(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response', {'data': 'Room ' + message['room'] + ' is closing.',
-                         'count': session['receive_count']},
-         room=message['room'])
-    close_room(message['room'])
-
-
-@socketio.on('my_room_event', namespace='/test')
-def send_room_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']},
-         room=message['room'])
-
-
-@socketio.on('disconnect_request', namespace='/test')
-def disconnect_request():
-    @copy_current_request_context
-    def can_disconnect():
-        disconnect()
-
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    # for this emit we use a callback function
-    # when the callback function is invoked we know that the message has been
-    # received and it is safe to disconnect
-    emit('my_response',
-         {'data': 'Disconnected!', 'count': session['receive_count']},
-         callback=can_disconnect)
-
-
-@socketio.on('my_ping', namespace='/test')
-def ping_pong():
-    emit('my_pong')
 
 
 @socketio.on('connect', namespace='/test')
@@ -111,11 +73,6 @@ def test_connect():
         if thread is None:
             thread = socketio.start_background_task(background_thread)
     emit('my_response', {'data': 'Connected', 'count': 0})
-
-
-@socketio.on('disconnect', namespace='/test')
-def test_disconnect():
-    print('Client disconnected', request.sid)
 
 
 if __name__ == '__main__':
